536447e2dc62c806ff7fb21b249a3ac6cdf0b597
yisus-Developer16 <Jesus.Colque@fundacion-jala.org>
2023-04-27 20:10:38 -0400

    Class_number_1
    Imprimiendo con la función PRINT el resultado del método "draw_background()" para corroborar que se aplica correctamente en el eje X
    
     Y el 2do cambio cambiar el valor de la cadena de txt de "Chrome Dino Runner" a "Dino Runner Game"

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -47,3 +47,4 @@ class Game:
             self.screen.blit(BG, (image_width + self.x_pos_bg, self.y_pos_bg))
             self.x_pos_bg = 0
         self.x_pos_bg -= self.game_speed
+        print(self.x_pos_bg)
